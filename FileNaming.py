24a569eef7ed2c97deb24ce7f8a961d2733be817
UrsidaeWasTaken <44565582+UrsidaeWasTaken@users.noreply.github.com>
2019-04-07 14:51:22 +0100

    Added "File Naming" solution

--- FileNaming.py
+++ FileNaming.py
@@ -1,5 +1,7 @@
 """
-You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet. Return an array of names that will be given to the files.
+You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names,
+the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such
+that the obtained name is not used yet. Return an array of names that will be given to the files.
 
 For example, given the array ["doc", "doc", "image", "doc(1)", "doc"], return ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
 """
