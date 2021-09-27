7ca0ea3955acd7bf8388a6aacffe5ac42511d533
UrsidaeWasTaken <ursidaemail@gmail.com>
2021-09-27 19:52:29 +0100

    Added 'Flatland Space Station' Description

--- Javascript/flatlandSpaceStations.js
+++ Javascript/flatlandSpaceStations.js
@@ -1,3 +1,17 @@
+/*
+Flatland is a country with a number of cities, some of which have space stations. Cities are numbered consecutively and each has a road of 1km length connecting it to the next city. It is not a circular route, so the first city doesn't connect with the last city.
+
+Determine the maximum distance from any city to its nearest space station.
+
+For example, given n = 3 cities where c = [1] are space stations, only City 1 has a space station. They occur consecutively along a route. City 0 is 1-0=1 unit away and city 2 is 2-1=1 unit away. City 1 is 1-1=0 units from its nearest space station as one is located there. The maximum distance is 1.
+
+flatlandSpaceStations has the following parameter(s):
+
+    int n: the number of cities
+    int c[m]: the indices of cities with a space station
+
+*/
+
 function flatlandSpaceStations(n, c) {
     let sortedArr = new Int32Array(c).sort();
     
